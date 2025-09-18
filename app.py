5b3cbfa4338908f6b6bb1cd8258bfd7a3229b732
Voroshilova Yelizaveta <liza21012005uk@gmail.com>
2025-09-19 02:42:28 +0700

    Исправление адресов

--- app.py
+++ app.py
@@ -7,20 +7,20 @@ def not_found(err):
     return "Нет такой страницы", 404
 
 @app.route("/")
-@app.route("/web")
+@app.route("/lab1/web")
 def web():
     return """<!doctype html>
         <html>
             <body>
                 <h1>web-сервер на flask</h1>
-                <a href="/author">author</a>
+                <a href="/lab1/author">author</a>
             </body>
         </html>""", 200, {
             'X-Server': 'sample',
             'Content-Type': 'text/plain; charset=utf-8'
         }
 
-@app.route("/author")
+@app.route("/lab1/author")
 def author():
     name = "Ворошилова Елизавета Андреевна"
     group = "ФБИ-34"
@@ -32,11 +32,11 @@ def author():
                 <p>Студент: """ + name + """</p>
                 <p>Группа: """ + group + """</p>
                 <p>Факультет: """ + faculty + """</p>
-                <a href="/web"> web </a>
+                <a href="/lab1/web"> web </a>
             </body>
         </html>"""
 
-@app.route("/image")
+@app.route("/lab1/image")
 def image ():
     path = url_for("static", filename="cru.jpg")
     css_path = url_for("static", filename="lab1.css")
@@ -53,9 +53,9 @@ def image ():
                     <h1 class="image-title">Цыпленок</h1>
                     <img src="''' + path + '''" class="styled-image" alt="Цыпленок">
                     <div class="navigation">
-                        <a href="/web" class="nav-link">На главную</a>
-                        <a href="/author" class="nav-link">Об авторе</a>
-                        <a href="/counter" class="nav-link">Счетчик</a>
+                        <a href="/lab1/web" class="nav-link">На главную</a>
+                        <a href="/lab1/author" class="nav-link">Об авторе</a>
+                        <a href="/lab1/counter" class="nav-link">Счетчик</a>
                     </div>
                 </div>
             </div>
@@ -64,7 +64,7 @@ def image ():
     '''
 count = 0
 
-@app.route('/counter')
+@app.route('/lab1/counter')
 def counter():
     global count
     count += 1
@@ -90,11 +90,11 @@ def counter():
 def clear_counter():
     global count
     count = 0
-    return redirect('/counter')
+    return redirect('/lab1/counter')
 
-@app.route("/info")
+@app.route("/lab1/info")
 def info():
-    return redirect("/author")
+    return redirect("/lab1/author")
 
 @app.route("/lab1/created")
 def created():
