9a12b27082fe8bf62f198983dd3eebfcaeb97a83
Voroshilova Yelizaveta <liza21012005uk@gmail.com>
2025-10-02 22:42:54 +0700

    Имя студента

--- app.py
+++ app.py
@@ -681,4 +681,5 @@ def add_flower (name):
 
 @app.route('/lab2/example')
 def example():
-    return render_template('example.html')
+    name = 'Ворошилова Елизавета'
+    return render_template('example.html', name=name)
