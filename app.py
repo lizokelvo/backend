60a39c31c2cdb7c55e62b9163bd94bfff801f2ee
Voroshilova Yelizaveta <liza21012005uk@gmail.com>
2025-10-03 19:19:22 +0700

    Цикл в шаблоне2

--- app.py
+++ app.py
@@ -681,12 +681,14 @@ def add_flower (name):
 
 @app.route('/lab2/example')
 def example():
-    name = ''
-    lab_num = ''
-    group = 'ФБИ-34' 
-    course = '3 курс'      
-    return render_template('example.html', 
-                           name=name, 
-                           lab_num=lab_num, 
-                           group=group, 
-                           course=course)
+    name, lab_num, group, course = 'Ворошилова Елизавета', 2, 'ФБИ-34', 3
+    fruits = [
+        {'name': 'яблоки', 'price': 100},
+        {'name': 'груши', 'price': 120},
+        {'name': 'апельсины', 'price': 80},
+        {'name': 'мандарины', 'price': 95},
+        {'name': 'манго', 'price': 321}
+    ]
+    return render_template('example.html',
+                            name=name, lab_num=lab_num, group=group,
+                            course=course, fruits=fruits)
