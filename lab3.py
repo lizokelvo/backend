b0314defbce9e4219aa3e8f518ee6af260ee3d13
Voroshilova Yelizaveta <liza21012005uk@gmail.com>
2025-11-01 13:27:35 +0700

    1

---
+++ lab3.py
@@ -0,0 +1,10 @@
+from flask import Blueprint, url_for, request, redirect, abort, render_template # type: ignore
+import datetime
+
+lab3 = Blueprint('lab3', __name__)
+
+
+@lab3.route('/lab3/')
+def lab():
+    return render_template('lab3/lab3.html')
+
