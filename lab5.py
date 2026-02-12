d3360285e93c5e8b96cb204838eb52dfa8f441de
Voroshilova Yelizaveta <liza21012005uk@gmail.com>
2026-02-13 00:23:00 +0700

    1213

--- lab5.py
+++ lab5.py
@@ -5,25 +5,20 @@ lab5 = Blueprint('lab5', __name__)
 
 @lab5.route('/lab5/')
 def lab5_index():
-    """Главная страница лабораторной работы 5"""
     return render_template('lab5/lab5.html')
 
 @lab5.route('/lab5/login', methods=['GET', 'POST'])
-def login():
-    """Страница входа"""   
+def login():  
     return render_template('lab5/login.html')
 
 @lab5.route('/lab5/register', methods=['GET', 'POST'])
 def register():
-    """Страница регистрации"""
     return render_template('lab5/register.html')
 
 @lab5.route('/lab5/list')
 def list_articles():
-    """Список статей"""
     return render_template('lab5/list.html')
 
 @lab5.route('/lab5/create', methods=['GET', 'POST'])
 def create_article():
-    """Создание статьи"""
     return render_template('lab5/create.html')
