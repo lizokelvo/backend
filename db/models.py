23f325f40f982443affd983284f8199d2351c4b4
Voroshilova Yelizaveta <liza21012005uk@gmail.com>
2026-02-18 15:54:52 +0700

    models.py

---
+++ db/models.py
@@ -0,0 +1,19 @@
+from . import db
+
+class User(db.Model):
+    __tablename__ = 'users'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    login = db.Column(db.String(30), nullable=False, unique=True)
+    password = db.Column(db.String(162), nullable=False)
+
+class Article(db.Model):
+    __tablename__ = 'articles'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    login_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    title = db.Column(db.String(50), nullable=False)
+    article_text = db.Column(db.Text, nullable=False)
+    is_favorite = db.Column(db.Boolean, default=False)
+    is_public = db.Column(db.Boolean, default=True)
+    likes = db.Column(db.Integer, default=0)
