1dea03207788b3def5590552c5a58428fe80f1ca
Voroshilova Yelizaveta <liza21012005uk@gmail.com>
2026-02-18 12:27:34 +0700

    Заготовка к 8-ой лабораторной

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from lab4 import lab4
 from lab5 import lab5
 from lab6 import lab6
 from lab7 import lab7
-#from lab8 import lab8
+from lab8 import lab8 
 #from lab9 import lab9
 import datetime
 import os
@@ -20,7 +20,7 @@ app.register_blueprint(lab4)
 app.register_blueprint(lab5)
 app.register_blueprint(lab6)
 app.register_blueprint(lab7)
-#app.register_blueprint(lab8)
+app.register_blueprint(lab8)
 #app.register_blueprint(lab9)
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'my_secret_key_147852369')
